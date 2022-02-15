0dd47623996b45af77cee1d5a8357cd1ee95cdec
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2022-02-15 09:36:20 +0100

    updating email and phone in appreducer

--- src/_consts/colors/colors.js
+++ src/_consts/colors/colors.js
@@ -3,9 +3,5 @@ export const colors = {
   red: "#F16653",
   gold: "#FEC100",
   green: "#A0C76E",
-<<<<<<< HEAD
-  grey: "#91908E",
-=======
   grey: "#5e5e5e",
->>>>>>> 865829f8e96bc9be2a8e1833a9971b09e5d265cc
 };
